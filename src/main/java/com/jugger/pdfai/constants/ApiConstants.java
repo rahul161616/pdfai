7ce56ea9d3452ff1d5dd4168062fa936ea73d367
rahul161616 <rahulsewa1616@gmail.com>
2026-03-24 17:15:30 +0545

    chore: setup files added

---
+++ src/main/java/com/jugger/pdfai/constants/ApiConstants.java
@@ -0,0 +1,8 @@
+package com.jugger.pdfai.constants;
+
+public class ApiConstants {
+    public static final String API_BASE_PATH = "/api/v1";
+    public static final String DOCUMENTS = API_BASE_PATH + "/documents";
+    public static final String UPLOAD_FILE_PATH = "/upload";
+    public static final String EXTRACT_TEXT_PATH = "/extract";
+}
